eaedada37fa70ec5cb139fe1f45226c00a6fa6c6
yxyyds666 <138752054+yxyyds666@users.noreply.github.com>
2023-09-04 14:13:13 +0800

    Add files via upload

---
+++ iSH-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "安装iSH-Tools"
+apk update #更新
+apk add wget bash #安装工具
+wget https://github.com/lurenJBD/iSH-Tools/raw/main/iSH-Tools.sh -qO iSH-Tools.sh #github拉取iSH工具
+bash iSH-Tools.sh #运行
+5 #安装脚本
+q #退出
+iSH-Tools #运行脚本
+3 #更改镜像源
+1
+3 #选择中科大
+y #确定
+y #覆盖
+q #退出
+q #退出
+echo "安装完成.镜像源更改为中科大.工具输入:iSHTools"
